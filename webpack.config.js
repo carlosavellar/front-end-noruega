90785de601cc583ae5ce977026deb4d5b041b105
carlosavellar <carlos.avellar@gmail.com>
2023-08-22 18:44:05 -0300

    Page structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/js/main.js",
   output: {
-    filename: "main.js",
+    filename: "main-bandle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
@@ -19,31 +19,5 @@ module.exports = {
     static: path.resolve(__dirname, "src"),
     port: 8080,
     hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: () => [require("autoprefixer")],
-              },
-            },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-    ],
   },
 };
